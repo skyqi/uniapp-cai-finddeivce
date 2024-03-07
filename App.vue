0b0d55705f5177c03561ad2b5984cc3737003fc0
skyqi035 <skyqi035@hotmail.com>
2024-03-07 19:27:07 +0800

    submit

---
+++ App.vue
@@ -0,0 +1,52 @@
+<script>	
+	import { getTheme } from '@/common/utils';
+	const theme = getTheme()
+ 
+	// @import '@/common/'+theme+'/uni.css';
+	
+	function loadcss(cssPath) {
+		const style = document.createElement('style');
+		const link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = cssPath;
+		document.head.appendChild(link);
+	}
+	
+	export default {
+		onLaunch: function() {
+			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
+			loadcss('/common/'+theme+'/uni.css')
+			loadcss('/common/'+theme+'.css')
+			console.log('/common/'+theme+'/uni.css')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		} 
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import '@/uni_modules/uni-scss/index.scss';
+	// @import '@/common/uni.css';
+	@import '@/common/main.css';
+	/* #ifndef APP-NVUE */
+	@import '@/static/customicons.css';
+	// 设置整个项目的背景色
+	page {
+		background-color: var(--main-bg-color);
+		color:var(--dark-white-light-block);
+		
+	}
+	 
+	/* #endif */
+	.example-info {
+		font-size: 14px;
+		color: #333;
+		padding: 10px;
+	}
+</style>
---
+++ dev.md
@@ -0,0 +1,58 @@
+[](https://uniapp.dcloud.net.cn/component/uniui/uni-popup.html#type-options)
+
+[](http://192.168.1.1:5173/#/pages/adddevice/adddevice)
+
+'''
+uni.navigateTo({
+    url: '/pages/about/about'
+});
+  
+returnHandle() {
+   uni.navigateBack();
+}
+'''
+
+
+
+'''
+.container {
+  position: relative;
+  width: 100px;
+  height: 100px;
+}
+
+.container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  
+  -webkit-mask-image: url("image.svg");
+  mask-image: url("image.svg");
+  mask-mode: alpha;
+  mask-repeat: no-repeat;
+  mask-position: center;
+}
+
+.container::before {
+  fill: red;
+}
+
+import { getTheme } from '@/common/utils'; // 只导入特定的函数
+
+const Theme = getTheme()
+	
+Theme:getTheme()
+
+<img class="loading" :src="'/static/gifs/'+theme+'/Radar-E.gif'" /> 
+
+<view class="aboutview pageview">
+			<span @click='returnHandle'>
+				<em class="nav-arrow"></em>
+			</span>
+</uni-section>
+			
+
+'''
